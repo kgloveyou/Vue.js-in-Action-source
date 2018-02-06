8dba7b442d9840f25c4993eb27fbff291cc6752a
hanjf <hanjf@infore.com>
2018-02-06 13:03:05 +0800

    modify

--- chapter8/8.2.2/time.js
+++ chapter8/8.2.2/time.js
@@ -2,10 +2,12 @@
  * Created by Administrator on 2018-01-25.
  */
 var Time={
+  //获取当前时间戳
   getUnix:function () {
     var date=new Date();
     return date.getTime();
   },
+  //获取今天0点0分0秒的时间戳
   getTodayUnix:function () {
     var date=new Date();
     date.setHours(0);
@@ -14,6 +16,7 @@ var Time={
     date.setMilliseconds(0);
     return date.getTime();
   },
+  //获取今年1月1日0时0分0秒的时间戳
   getYearUnix:function () {
     var date=new Date();
     date.setMonth(0);
@@ -24,17 +27,19 @@ var Time={
     date.setMilliseconds(0);
     return date.getTime();
   },
+  //获取标准年月日
   getLastDate:function (time) {
     var date=new Date(time);
     var month=date.getMonth()+1<10?'0'+(date.getMonth()+1):date.getMonth()+1;
     var day=date.getDate()+1<10?'0'+date.getDate():date.getDate();
     return date.getFullYear()+"-"+month+'-'+day;
   },
+  //转换时间
   getFormatTime:function (timestamp) {
     var now=this.getUnix();
-    var tody=this.getTodayUnix();
+    var today=this.getTodayUnix();
     var year=this.getYearUnix();
-    var timer=(now-timestamp)/1000;
+    var timer=(now-timestamp)/1000;//转换为秒级时间戳
 
     var tip='';
 
@@ -44,7 +49,7 @@ var Time={
       tip='刚刚';
     }else if(timer<3600){
       tip=Math.floor(timer/60)+'分钟前';
-    }else if(timer>=3600&&(timestamp-tody>=0)){
+    }else if(timer>=3600&&(timestamp-today>=0)){
       tip=Math.floor(timer/3600)+'小时前';
     }else if(timer/86400<=31){
       tip=Math.Ceil(timer/86400)+'天前';
